5be8c4ec189f70369ba027b7c3a46504b7c813cc
Angelo <angelopaolosantos@gmail.com>
2018-04-21 22:38:31 -0700

    Lint webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ const Common = merge([
       // The context property references the source directory and tells
       // webpack to begin there. `main` is just the key that references
       // the starting point of the application, `index.js`
-      main: './index.js',
+      main: './index.js'
     },
     output: {
       // `[name]` will be replaced with the key that references our
